4e4fcd4eda2dd3a94fa2bdfd2a3d9ac4491904c1
Laurence Bahiirwa <laurence@mediauganda.com>
2020-05-05 12:58:48 +0300

    first commit

--- crosspost.php
+++ crosspost.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name: Latest Github Release
- * Description: Automatically add a download link to the latest Github repo release zips with a shortcode like [latest_github_release user="github" repo="hub"]
- * Version: 2.0.0
- * Author: Laurence Bahiirwa, James Nylen
+ * Plugin Name: Crosspost
+ * Description: Automatically add latest posts from other WordPress sites with a shortcode like [crosspost url="example.com" postnumber="3"]
+ * Version: 0.1.0
+ * Author: Laurence Bahiirwa 
  * Author URI: https://omukiguy.com
- * Plugin URI: https://github.com/bahiirwa/latest-github-release
- * Text Domain: latest_github_release
+ * Plugin URI: https://External Website.com/bahiirwa/crosspost
+ * Text Domain: crosspost
  * Requires at least: 4.9
- * Tested up to: 5.3.2
+ * Tested up to: 5.4.1
  * 
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
@@ -17,14 +17,14 @@
  *
  */
 
-namespace bahiirwa\LatestGithubRelease;
+namespace bahiirwa\Crosspost;
 
 // Prevent direct access.
 if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
-class LatestGithubRelease {
+class Crosspost {
 	/**
 	 * Add action to process shortcodes.
 	 *
@@ -33,7 +33,7 @@ class LatestGithubRelease {
 	 *
 	 */
 	public static function register() {
-		add_shortcode( 'latest_github_release', [ __CLASS__, 'process_shortcode' ] );
+		add_shortcode( 'crosspost', [ __CLASS__, 'process_shortcode' ] );
 	}
 
 	/**
@@ -50,50 +50,53 @@ class LatestGithubRelease {
 	public static function process_shortcode( $atts ) {
 		// Default values for when not passed in shortcode.
 		$defaults = [
-			'user'  => '',
-			'repo'  => '',
-			'name'  => 'Download Zip', // default button name
-			'class' => 'latest-github-release-link',
+			'url'  => '',
+			'characters'  => '150',
+			'readmoretext'  => 'Read more',
+			'postnumber'  => '3'
 		];
 
 		// Replace any missing shortcode arguments with defaults.
 		$atts = shortcode_atts(
 			$defaults,
 			$atts,
-			'latest_github_release'
+			'crosspost'
 		);
 
 		// Validate the user and the repo.
-		if ( empty( $atts['user'] ) || empty( $atts['repo'] ) ) {
-			return '<!-- [latest_github_release] Missing user or repo! -->';
+		if ( empty( $atts['url'] ) ) {
+			return '<p>[crosspost] Missing URL</p>';
 		}
 
-		// Get the release data from GitHub.
+		// Get the release data from External Website.
 		$release_data = self::get_release_data_cached( $atts );
 		if ( is_wp_error( $release_data ) ) {
 			return (
-				'<!-- [latest_github_release] '
+				'<!-- [crosspost] '
 				. esc_html( $release_data->get_error_message() )
 				. ' -->'
 			);
 		}
-
-		// Get the URL to a release zipfile.
-		$zip_url = self::get_zip_url_for_release( $release_data );
-		if ( is_wp_error( $zip_url ) ) {
-			return (
-				'<!-- [latest_github_release] '
-				. esc_html( $zip_url->get_error_message() )
-				. ' -->'
+			
+		foreach ( $release_data as $data ) {
+			// echo '<pre>';
+			// var_dump( $data );
+			// echo '</pre>';
+			
+			$html .= (
+				'<div class="crosspost-plugin" id="' . $data['id'] . '">' .
+					'<img class="featured-image" src="' . $data['featured_image_src'] . '" />' .
+					'<h3>' . $data['title']['rendered'] . '</h3>' .
+					'<div class="content">' . self::reduce_content( $data['content']['rendered'], $atts['characters'] ) . '</div>' .
+					'<div class="post-meta">' .
+						'<span class="date">' . self::convert_date_to_human( $data['date'] ) . '</span>' .
+						'<span class="author">' . $data['author_info']['display_name'] . '</span>' .
+					'</div>' .
+					'<a href="' . $data['link'] . '">' . $atts['readmoretext'] . '</a>' .
+				'</div>' 
 			);
+				
 		}
-
-		$html = (
-			'<a href="' . esc_attr( $zip_url ) . '"'
-			. ' class="' . esc_attr( $atts['class'] ) . '">'
-			. esc_html( $atts['name'] )
-			. '</a>'
-		);
 
 		/**
 		 * Filters the HTML for the release link.
@@ -103,16 +106,26 @@ class LatestGithubRelease {
 		 * @param string $html The link HTML.
 		 * @param array  $atts The full array of shortcode attributes.
 		 */
-		return apply_filters( 'latest_github_release_link', $html, $atts );
+		return apply_filters( 'crosspost_link', $html, $atts );
+	}
+
+	public static function reduce_content( $string, $characters ) {
+		if ( strlen( $string ) > 10 ) {
+			return esc_textarea( sanitize_text_field( $string = substr( $string , 0, $characters ) ) ); 
+		}
+	}
+
+	public static function convert_date_to_human( $date ) {
+		return $date = date( 'l jS M Y g:ia ', strtotime( $date ) );
 	}
 
 	/**
-	 * Fetch release data from GitHub or return it from a cached value.
+	 * Fetch release data from External Website or return it from a cached value.
 	 *
 	 * @since 2.0.0
 	 *
 	 * @param array $atts Array containing 'user' and 'repo' arguments.
-	 * @return array|\WP_Error Release data from GitHub, or an error object.
+	 * @return array|\WP_Error Release data from External Website, or an error object.
 	 */
 	public static function get_release_data_cached( $atts ) {
 		// Get any existing copy of our transient data
@@ -150,27 +163,24 @@ class LatestGithubRelease {
 	 */
 	public static function get_transient_name( $atts ) {
 		return (
-			'lgr_api_'
-			. substr( md5( $atts['user'] . '/' . $atts['repo'] ), 0, 16 )
+			'crosspost_link_'
+			. substr( md5( $atts['url'] ), 0, 16 )
 		);
 	}
 
 	/**
-	 * Fetch release data from GitHub.
+	 * Fetch release data from External Website.
 	 *
 	 * @since 2.0.0
 	 *
 	 * @internal - use self::get_release_data_cached() instead.
 	 *
 	 * @param array $atts Array containing 'user' and 'repo' arguments.
-	 * @return array|\WP_Error Release data from GitHub, or an error object.
+	 * @return array|\WP_Error Release data from External Website, or an error object.
 	 */
 	private static function get_release_data( $atts ) {
-		// Build the GitHub API URL for the latest release.
-		$api_url = (
-			'https://api.github.com/repos/'
-			. $atts['user'] . '/' . $atts['repo']
-			. '/releases/latest'
+		// Build the External Website API URL for the latest release.
+		$api_url = ( $atts['url'] . '/wp-json/wp/v2/posts'
 		);
 
 		// Make API call.
@@ -188,7 +198,7 @@ class LatestGithubRelease {
 		if ( empty( $response_json ) || $status_code !== 200 ) {
 			return new \WP_Error(
 				'invalid_data',
-				'Invalid data returned from GitHub',
+				'Invalid data returned from External Website',
 				[
 					'code' => $status_code,
 					'body' => empty( $response_json ) ? $response_body : $response_json,
@@ -200,38 +210,38 @@ class LatestGithubRelease {
 	}
 
 	/**
-	 * Given a set of release data from the GitHub API, return a release zip URL.
+	 * Given a set of release data from the External Website API, return a release zip URL.
 	 *
 	 * @since 2.0.0
 	 *
-	 * @param array $release_data Release data from the GitHub API.
-	 * @return string URL of latest zip release file on GitHub.
+	 * @param array $release_data Release data from the External Website API.
+	 * @return string URL of latest zip release file on External Website.
 	 */
-	public static function get_zip_url_for_release( $release_data ) {
-		// If any files were uploaded for this release, use the first one.
-		// TODO: Allow specifying which file to use somehow (name regex?)
-		if ( ! empty( $release_data['assets'] ) ) {
-			return $release_data['assets'][0]['browser_download_url'];
-		}
+	// public static function get_zip_url_for_release( $release_data ) {
+	// 	// If any files were uploaded for this release, use the first one.
+	// 	// TODO: Allow specifying which file to use somehow (name regex?)
+	// 	if ( ! empty( $release_data['assets'] ) ) {
+	// 		return $release_data['assets'][0]['browser_download_url'];
+	// 	}
 
-		// Otherwise, build a URL based on the tag name of the latest release.
-		$version = $release_data['tag_name'];
+	// 	// Otherwise, build a URL based on the tag name of the latest release.
+	// 	$version = $release_data['tag_name'];
 
-		// Extract the user and repo name from the GitHub API URL.
-		preg_match(
-			'#^https://api\.github\.com/repos/([^/]+)/([^/]+)/releases/#',
-			$release_data['url'],
-			$matches
-		);
-		$user = $matches[1];
-		$repo = $matches[2];
+	// 	// Extract the user and repo name from the External Website API URL.
+	// 	preg_match(
+	// 		'#^https://api\.External Website\.com/repos/([^/]+)/([^/]+)/releases/#',
+	// 		$release_data['url'],
+	// 		$matches
+	// 	);
+	// 	$user = $matches[1];
+	// 	$repo = $matches[2];
 
-		return (
-			'https://github.com/'
-			. $user . '/' . $repo
-			. '/archive/' . $version . '.zip'
-		);
-	}
+	// 	return (
+	// 		'https://External Website.com/'
+	// 		. $user . '/' . $repo
+	// 		. '/archive/' . $version . '.zip'
+	// 	);
+	// }
 }
 
-LatestGithubRelease::register();
+Crosspost::register();
